5fb854ce85f28034ba645551fc35547d9a1d62d3
Anmol Goel <agoel00@gmail.com>
2018-12-28 17:10:29 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import './App.css'
+import GA from './genetics/algorithm'
+
+class App extends Component {
+
+  state = {
+    problem: "the infinite monkey theorem states that given an infinite amount time, an infinite number of monkeys hitting keys at random could recreate any work such as the complete volumes of William Shakespeare.",
+    solution: null,
+  }
+
+  componentDidMount() {
+    this.ga = new GA(this.state.problem)
+    this.setState({
+      population: this.ga.seed(1000)
+    })
+  }
+
+  renderSolution() {
+    if (!this.state.solution) return
+    return this.state.solution.genes.map(letter => <span className="letter">
+      { letter.toString() }
+    </span>)
+  }
+
+  solve = () => {
+    const { problem, solution } = this.state
+
+    setTimeout(() => {
+      this.setState(prevState => ({
+        population: this.ga.breedGeneration(prevState.population),
+        solution: this.ga.bestInPopulation(prevState.population)
+      }))
+
+      if(this.state.solution.toString() !== problem) {
+        this.solve()
+      }
+      
+    }, 10)
+  }
+
+  updateProblem = (e) => {
+    this.ga = new GA(e.target.value)
+    this.setState({
+      population: this.ga.seed(1000)
+    })
+
+    this.setState({
+      problem: e.target.value
+    })
+  }
+
+  render() {
+    return <div className="container">
+    <h1>Genetic algorithm</h1>
+      <div>
+        { this.renderSolution() }
+      </div>
+
+      <textarea
+        type="text"
+        value={this.state.problem}
+        onChange={ this.updateProblem }
+        rows={4}
+        cols={80}
+        className="textArea"
+      />
+
+      <div className="containerBtn">
+        <button
+          onClick={this.solve}
+          className="pulse-button"
+        >Solve!</button>
+      </div>
+    </div>
+  }
+}
+
+export default App
